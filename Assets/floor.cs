04c5885bf4b130c8eff3cc6c8f0ded3fa7981596
swilliz <Sam@D-10-156-1-38.dhcp4.washington.edu>
2016-05-19 17:27:39 -0700

    setUpTest

---
+++ Assets/floor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+			//test
+		Debug.LogErrorFormat("hi");
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
